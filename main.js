6d26ce85f5c7ee5b8e39adc3a827e8bb83010438
sairabhanu965@gmail.com <sairabhanu965@gmail.com>
2018-07-07 13:46:23 +0530

    addingfiles

--- main.js
+++ main.js
@@ -12,6 +12,36 @@ function getfile(file,callback) {
 getfile("data.json",function(text){
   let data = JSON.parse(text);
   console.log(data);
-
+ career(data.career);
+ education(data.education);
 }
 )
+var child2=document.querySelector(".childtwo");
+
+function career(careerInfo){
+console.log(careerInfo);
+var careerHeading=document.createElement("h3");
+child2.appendChild(careerHeading);
+careerHeading.textContent="career objective";
+
+var careerHr=document.createElement("hr");
+child2.appendChild(careerHr);
+
+
+var ch=document.createElement("p");
+ ch.textContent=careerInfo.info;
+ child2.appendChild(ch);
+}
+function education(edu){
+  var eduHeading=document.createElement("h3");
+  eduHeading.textContent="Educational Qualifications";
+  child2.appendChild(eduHeading);
+
+  var careerHr=document.createElement("hr");
+  child2.appendChild(careerHr);
+var eduTable=document.createElement("table");
+eduTable.border="1";
+var tr1="<tr><td>Degree</td><td>Institute</td><td>Data</td></tr>";
+eduTable.innerHTML=tr1;
+child2.appendChild(eduTable);
+}
